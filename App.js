3588b43f35bda4a78908b8fd38760477a3df9e0d
fa.andrade <fabricio-andrade14@hotmail.com>
2024-06-07 18:47:30 -0300

    notif

--- App.js
+++ App.js
@@ -1,16 +1,68 @@
-import * as React from 'react';
-import { Button, View, StyleSheet } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, View, StyleSheet, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Products from './components/Products';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  }),
+});
 
 function HomeScreen({ navigation }) {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [channels, setChannels] = useState([]);
+  const [notification, setNotification] = useState(undefined);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => token && setExpoPushToken(token));
+
+    if (Device.OS === 'android') {
+      Notifications.getNotificationChannelsAsync().then(value => setChannels(value ?? []));
+    }
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(notificationListener.current);
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button
         onPress={() => navigation.navigate('Produtos')}
         title="Ver produtos"
       />
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}>
+        
+        <Button
+          title="Press to schedule a notification"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -18,7 +70,7 @@ function HomeScreen({ navigation }) {
 function NotificationsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Products/>
+      <Products />
     </View>
   );
 }
@@ -26,6 +78,8 @@ function NotificationsScreen({ navigation }) {
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  
+
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
@@ -35,6 +89,73 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "notificação",
+      body: 'Funcionando',
+      data: { data: 'goes here', test: { test1: 'more data' } },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Device.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    // EAS projectId is used here.
+    try {
+      const projectId =
+        Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+      if (!projectId) {
+        throw new Error('Project ID not found');
+      }
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(token);
+    } catch (e) {
+      token = `${e}`;
+    }
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
+}
+
+Notifications.scheduleNotificationAsync({
+  content: {
+    title: 'Look at that notification',
+    body: "I'm so proud of myself!",
+  },
+  trigger: null,
+});
 
 const styles = StyleSheet.create({
   container: {
--- components/Products.js
+++ components/Products.js
@@ -9,18 +9,19 @@ const Separator = () => {
 
 function ProductsList({ data, deleteItem, editItem }) {
     return (
-        <View style={styles.containerCard}>
-            <Text style={styles.text}>{data.nome}</Text>
-            <Text style={styles.text}>Desenvolvido por {data.prod}</Text>
-            <Text style={styles.text}>Categoria: {data.categoria}</Text>
-            <Text style={styles.text}>Ano de lançamento: {data.ano}</Text>
-
-            <View style={styles.item}>
+        <View style={styles.Card}>
+            <View style={styles.cardContent}>
+                <Text style={[styles.text, styles.cardText]}>{data.nome}</Text>
+                <Text style={[styles.text, styles.cardText]}>Desenvolvido por {data.prod}</Text>
+                <Text style={[styles.text, styles.cardText]}>Categoria: {data.categoria}</Text>
+                <Text style={[styles.text, styles.cardText]}>Ano de lançamento: {data.ano}</Text>
+            </View>
+            <View style={styles.cardOptions}>
                 <Pressable onPress={() => deleteItem(data.key)}>
-                    <Text style={styles.text} >Excluir</Text>
+                    <Text style={[styles.optionsText, styles.optionsDel]}>Excluir</Text>
                 </Pressable>
                 <Pressable onPress={() => editItem(data)}>
-                    <Text style={styles.text} >Editar</Text>
+                    <Text style={[styles.optionsText, styles.optionsEdit]} >Editar</Text>
                 </Pressable>
             </View>
         </View>
@@ -122,47 +123,45 @@ export default function Products() {
         setProducts("");
     }
     return (
-        cadastrar ? (
-            <View style={styles.container}>
-                <View style={styles.content}>
-                    <Pressable onPress={() => { setCad(false) }}>
-                        <Text>X</Text>
-                    </Pressable>
-                    <View style={styles.form}>
-                        <Text style={styles.formTitle}>Título</Text>
-                        <TextInput style={styles.Input} onChangeText={(text) => setName(text)} placeholder="Digite aqui" value={name} />
-                        <Separator />
-                        <Text style={styles.formTitle}>Desenvolvedora</Text>
-                        <TextInput style={styles.Input} onChangeText={(text) => setDev(text)} placeholder="Digite aqui" value={dev} />
-                        <Separator />
-                        <Text style={styles.formTitle}>Categoria</Text>
-                        <TextInput style={styles.Input} onChangeText={(text) => setCategoria(text)} placeholder="Digite aqui" value={categoria} />
-                        <Separator />
-                        <Text style={styles.formTitle}>Ano de lançamento</Text>
-                        <TextInput style={styles.Input} onChangeText={(text) => setAno(text)} placeholder="Digite aqui" value={ano} />
-                        <Separator />
-
-                    </View>
-                    {edit ?
-                        <Pressable style={styles.Pressable} onPress={editProduct}><Text style={styles.btnSave}>Alterar</Text></Pressable> :
-                        <Pressable style={styles.Pressable} onPress={addProduct}><Text style={styles.btnSave}>Salvar</Text></Pressable>
-                    }
-                </View>
-            </View>
-        ) : (<View style={styles.container}>
+    cadastrar ? (
+        <View style={styles.container}>
             <View style={styles.content}>
-                <Pressable onPress={() => { setCad(true) }}>
-                    <Text style={styles.btnSave}>Cadastrar</Text>
+                <Pressable onPress={() => { setCad(false) }}>
+                    <Text style={styles.text}>X</Text>
                 </Pressable>
-                <FlatList keyExtractor={item => item.key} data={products} renderItem={({ item }) => (
-                    <ProductsList data={item} editItem={handleEdit} deleteItem={() => handleDelete(item.id)} />
-                )} />
+                <View style={styles.form}>
+                    <Text style={styles.formTitle}>Título</Text>
+                    <TextInput style={styles.Input} onChangeText={(text) => setName(text)} placeholder="Digite aqui" value={name} />
+                    <Separator />
+                    <Text style={styles.formTitle}>Desenvolvedora</Text>
+                    <TextInput style={styles.Input} onChangeText={(text) => setDev(text)} placeholder="Digite aqui" value={dev} />
+                    <Separator />
+                    <Text style={styles.formTitle}>Categoria</Text>
+                    <TextInput style={styles.Input} onChangeText={(text) => setCategoria(text)} placeholder="Digite aqui" value={categoria} />
+                    <Separator />
+                    <Text style={styles.formTitle}>Ano de lançamento</Text>
+                    <TextInput style={styles.Input} onChangeText={(text) => setAno(text)} placeholder="Digite aqui" value={ano} />
+                    <Separator />
+
+                </View>
+                {edit ?
+                    <Pressable style={styles.Pressable} onPress={editProduct}><Text style={styles.btnSave}>Alterar</Text></Pressable> :
+                    <Pressable style={styles.Pressable} onPress={addProduct}><Text style={styles.btnSave}>Salvar</Text></Pressable>
+                }
             </View>
         </View>
-        )
-
-
-    );
+    ) : (<View style={styles.container}>
+        <View style={styles.content}>
+            <Pressable onPress={() => { setCad(true) }}>
+                <Text style={styles.btnSave}>Cadastrar</Text>
+            </Pressable>
+            <FlatList keyExtractor={item => item.key} data={products} renderItem={({ item }) => (
+                <ProductsList data={item} editItem={handleEdit} deleteItem={() => handleDelete(item.id)} />
+            )} />
+        </View>
+    </View>
+    )
+);
 }
 
 const styles = StyleSheet.create({
@@ -178,15 +177,49 @@ const styles = StyleSheet.create({
         margin: 15,
     },
 
-    containerCard: {
+    Card: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         marginVertical: 10,
         padding: 10,
         backgroundColor: "#444",
         width: "95vw",
         height: "15vh"
     },
-    text:{
+    cardContent:{
+        minWidth: '50%',
+        maxWidth: '50%'
+    },  
+
+    text: {
         color: "white"
+    },
+
+    optionsText: {
+        color: "white",
+        marginVertical: "10px",
+        marginRight: 15,
+        fontSize: 16,
+        width: 100,
+        height: 30,
+        textAlign: 'center',
+        paddingTop: 3
+    },
+
+    cardText:{
+        borderBottomColor: "#fff",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        marginVertical: 5,
+        
+    },
+
+    optionsEdit: {
+        backgroundColor: '#44f'
+    },
+    optionsDel: {
+        backgroundColor: '#f44'
     },
 
     form: {
@@ -195,7 +228,8 @@ const styles = StyleSheet.create({
 
     formTitle: {
         marginVertical: 5,
-        paddingHorizontal: 30
+        paddingHorizontal: 30,
+        color: '#fff'
     },
 
     Input: {
@@ -222,7 +256,7 @@ const styles = StyleSheet.create({
         width: "95vw",
         height: "6vh",
         //borderRadius: 100,
-        backgroundColor: "#88d"
+        backgroundColor: "#5b5"
     },
 
     separator: {
